afc95502d356f8558546089c2e3a69deb77b9527
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-12 09:08:57 -0400

    Primer commit

---
+++ src/main/java/com/example/inventorygamesback/InventoryGamesBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventorygamesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryGamesBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InventoryGamesBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/inventorygamesback/InventoryGamesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventorygamesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryGamesBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
