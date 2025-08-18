782afcea732a8c4c737e1e06ef00abf3e037ccef
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-18 11:14:14 -0400

    Limpieza en entidades

--- src/main/java/com/example/inventorygamesback/entity/Game.java
+++ src/main/java/com/example/inventorygamesback/entity/Game.java
@@ -1,18 +1,14 @@
 package com.example.inventorygamesback.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.sql.Timestamp;
-import java.util.List;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
